dab4d3f83a7d20c8ea104bab20820d7e70ae96b2
Marc-André Fortin <marcandre.fortin@gmail.com>
2022-12-06 19:47:34 -0500

    day 6

---
+++ day06/part2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define BUFSIZE 14
+
+int main()
+{
+    int c, i, j, k;
+    char buf[BUFSIZE];
+
+    c = getchar();
+    for (i = 0; i < BUFSIZE; ++i)
+        buf[i] = c;
+    k = 1;
+    while ((c = getchar()) != EOF && c != '\n') {
+        buf[k++ % BUFSIZE] = c;
+        for (i = 0; i < BUFSIZE-1; ++i) {
+            for (j = i+1; j < BUFSIZE; ++j)
+                if (buf[i] == buf[j])
+                    break;
+            if (j != BUFSIZE)
+                break;
+        }
+        if (j == BUFSIZE) {
+            printf("%d\n", k);
+            break;
+        }
+    }
+    return 0;
+}
